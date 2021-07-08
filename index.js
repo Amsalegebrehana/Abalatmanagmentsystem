699e2bfc30c18bcecce7f7e7bc788833c2dc0894
Selamawit926 <se.selamawit.elias@gmail.com>
2021-07-08 12:43:06 -0700

    Get route for getting all users done

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const pool= require('./queries');
 //const bodyParser = require('body-parser');
 const app = express();
-
-
 const port = 3000;
 
-// app.use(bodyParser.json())
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// )
-//route to look for a  get request
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-  })
+app.use(express.json());
+
+app.get('/', async(req, res) => {
+    // response.json({ info: 'Node.js, Express, and Postgres API' })
+    // console.log("Get working");
+    try{
+      const users= await pool.query("SELECT * FROM users");
+      res.json(users.rows);
+
+    }catch(err){
+      console.error(err.message);
+    }
+
+});
 
 //app to listen on port set
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+    console.log(`App running on port ${port}.`);
   })
--- queries.js
+++ queries.js
@@ -1,9 +1,11 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 //change this one to your database username and password
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
-  database: 'abalatdb',
   password: 'superuser',
-  port: 5432,
-})
+  database: 'abalatdb',
+  host: 'localhost',
+  port: 5432
+});
+
+module.exports=pool;
